67c191b680ab5246e9c606efb7771261b81d1a93
RyanGuinchard <ryanguinchard9@hotmail.com>
2024-06-03 21:01:21 -0230

    Basic form to test netlify forms

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from "react";
+import Button from "../../components/Button";
+import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div className="flex items-center justify-center h-screen text-7xl text-white">
-      Work in Progress
+    <div className="flex items-center justify-center h-screen text-2xl">
+      <form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        className="flex flex-col space-y-4"
+      >
+        <p>
+          <label>
+            Your Name: <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email: <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message: <textarea name="message"></textarea>
+          </label>
+        </p>
+        <p>
+          <span className="cursor-pointer">
+            <Button type="submit">Send</Button>
+          </span>
+        </p>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
