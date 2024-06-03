39e81d4c1056820703715b9b899169d99bd2fee5
RyanGuinchard <ryanguinchard9@hotmail.com>
2024-06-03 21:20:21 -0230

    attempt for netlify forms

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -32,11 +32,6 @@ const Contact = () => {
           </span>
         </p>
       </form>
-      <form name="contact" data-netlify="true" hidden>
-        <input type="text" name="name" />
-        <input type="email" name="email" />
-        <textarea name="message"></textarea>
-      </form>
     </div>
   );
 };
