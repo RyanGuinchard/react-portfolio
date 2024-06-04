0daf7ff77cc2566defbf544b1f658212e99a963d
RyanGuinchard <ryanguinchard9@hotmail.com>
2024-06-03 22:07:33 -0230

    Default button needed for form submission.

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,19 +1,19 @@
 import React from "react";
-import Button from "../../components/Button"
+import Button from "../../components/Button";
 import "./Contact.css";
 
 const Contact = () => {
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     const form = e.target;
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(new FormData(form)).toString()
+      body: new URLSearchParams(new FormData(form)).toString(),
     })
       .then(() => console.log("Form successfully submitted"))
-      .catch(error => alert(error));
+      .catch((error) => alert(error));
   };
 
   return (
@@ -42,7 +42,19 @@ const Contact = () => {
           </label>
         </p>
         <p>
-          <Button type="submit">Send</Button>
+          <button
+            type="submit"
+            className="
+          inline-block px-4 py-2 mt-4
+          text-sm font-medium text-white
+          bg-[#5C469C] border border-white border-opacity-[.08]
+          rounded-full
+          transition-colors duration-200
+          hover:bg-[#D4ADFC] hover:text-[#5C469C]
+      "
+          >
+            Send
+          </button>
         </p>
       </form>
     </div>
