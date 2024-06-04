1c41c44784c2d50e9cb34d3528e6e7150ced69f8
RyanGuinchard <ryanguinchard9@hotmail.com>
2024-06-03 21:54:19 -0230

    test2

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,8 +1,20 @@
 import React from "react";
-import Button from "../../components/Button";
 import "./Contact.css";
 
 const Contact = () => {
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(new FormData(form)).toString()
+    })
+      .then(() => console.log("Form successfully submitted"))
+      .catch(error => alert(error));
+  };
+
   return (
     <div className="flex items-center justify-center h-screen text-2xl">
       <form
@@ -10,6 +22,7 @@ const Contact = () => {
         method="POST"
         data-netlify="true"
         className="flex flex-col space-y-4"
+        onSubmit={handleSubmit}
       >
         <p>
           <label>
