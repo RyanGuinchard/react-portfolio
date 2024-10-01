a128a137dcbef4cf32e2e07fb122227b716daf26
Ryan Guinchard <ryanguinchard9@hotmail.com>
2024-10-01 14:14:56 -0230

    Changed Tech page to Projects only

--- src/pages/Tech/Tech.jsx
+++ src/pages/Tech/Tech.jsx
@@ -1,10 +1,6 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import mongodb from "../../assets/mongodb.png";
-import express from "../../assets/express.png";
-import react from "../../assets/react.png";
-import node from "../../assets/node.png";
 import tmdb from "../../assets/project1.png";
 import pets from "../../assets/project2.png";
 import df from "../../assets/project3.png";
@@ -46,68 +42,6 @@ const Tech = () => {
 
   return (
     <div className="tech flex flex-col justify-center items-center max-w-full sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto space-y-4">
-      <h1 className="text-5xl mb-5">Technology Stack</h1>
-      <div className="carousel-container max-w-full sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto">
-        <Carousel
-          showThumbs={false}
-          showStatus={false}
-          useKeyboardArrows={true}
-          swipeable={isSwipeable}
-          emulateTouch={true}
-        >
-          <div className="flex flex-col items-center">
-            <h2 className="text-3xl">MongoDb</h2>
-            <p className="text-2xl max-w-xl">
-              MongoDB is a general purpose, document-based, distributed database
-              built for modern application developers and for the cloud era.
-            </p>
-            <img
-              className="w-full h-32 sm:4-48 md:h-64 object-contain object-center max-w-[500px]"
-              src={mongodb}
-              alt="mongodb"
-            />
-          </div>
-          <div className="flex flex-col items-center">
-            <h2 className="text-3xl">Express</h2>
-            <p className="text-2xl max-w-xl">
-              Express is a minimal and flexible Node.js web application
-              framework that provides a robust set of features for web and
-              mobile applications.
-            </p>
-            <img
-              className="w-full h-32 sm:4-48 md:h-64 object-contain object-center max-w-[500px]"
-              src={express}
-              alt="express"
-            />
-          </div>
-          <div className="flex flex-col items-center">
-            <h2 className="text-3xl">React</h2>
-            <p className="text-2xl max-w-xl">
-              React is a JavaScript library for building user interfaces. It is
-              maintained by Facebook and a community of individual developers
-              and companies.
-            </p>
-            <img
-              className="w-full h-32 sm:4-48 md:h-64 object-contain object-center max-w-[500px]"
-              src={react}
-              alt="react"
-            />
-          </div>
-          <div className="flex flex-col items-center">
-            <h2 className="text-3xl">Node.js</h2>
-            <p className="text-2xl max-w-xl">
-              Node.js is an open-source, cross-platform, back-end JavaScript
-              runtime environment that runs on the V8 engine and executes
-              JavaScript code outside a web browser.
-            </p>
-            <img
-              className="w-full h-32 sm:4-48 md:h-64 object-contain object-center max-w-[500px]"
-              src={node}
-              alt="node"
-            />
-          </div>
-        </Carousel>
-      </div>
       <h1 className="text-5xl pt-20">Check out my Work!</h1>
       <div className="carousel-container max-w-full sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto">
         <Carousel
@@ -140,10 +74,10 @@ const Tech = () => {
           <div className="flex flex-col items-center">
             <h2 className="text-3xl">Safe Snacks For Special Pets</h2>
             <p className="text-2xl max-w-xl">
-              Discover what's safe for your pet! Our single-page web app lets
-              you quickly find out what your pet can or can't have based on
-              their type and the food you input. Keep your furry friend healthy
-              with ease.
+              Discover what&apos;s safe for your pet! Our single-page web app
+              lets you quickly find out what your pet can or can&apos;t have
+              based on their type and the food you input. Keep your furry friend
+              healthy with ease.
             </p>
             <img
               className="w-full h-32 sm:4-48 md:h-64 object-contain object-center max-w-[500px]"
@@ -163,8 +97,8 @@ const Tech = () => {
             <h2 className="text-3xl">DecisionForge</h2>
             <p className="text-2xl max-w-xl">
               DecisionForge is a game randomizer app that generates unique
-              playthrough options for games like Fallout 4, Baldur's Gate 3,
-              Cyberpunk 2077, and Pokémon. Built with React, Node.js, and
+              playthrough options for games like Fallout 4, Baldur&apos;s Gate
+              3, Cyberpunk 2077, and Pokémon. Built with React, Node.js, and
               MongoDB, it allows users to randomize character builds,
               playstyles, and more. Users can reroll, lock choices, and explore
               new gameplay challenges. Deployed on Netlify and Vercel, it
